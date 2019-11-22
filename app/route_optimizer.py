d78f2d69be4736c407db0aa3fb18813af34d4e12
Ekrekr <eliaskassell@gmail.com>
2019-11-22 20:15:23 +0000

    Added output formatting for routing input.

--- app/route_optimizer.py
+++ app/route_optimizer.py
@@ -1,5 +1,5 @@
 """
-PickemupFlask - gmaps
+PickemupFlask - route optimizer
 
 Communication with the gmaps API, which includes distance-matrix.
 """
@@ -9,7 +9,7 @@ from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
 
-class Routing:
+class RouteOptimizer:
     def __init__(self, solution_request, distance_matrix):
         """
         Solves the Pickemup routing problem.
@@ -21,17 +21,17 @@ class Routing:
         self.solution_request = solution_request
         self.distance_matrix = distance_matrix
 
-        self._data = {}
-        self._data["distance_matrix"] = self._calculate_matrix()
-        self._data["demands"] = self._calculate_demands()
-        starts, capacities = self._calculate_capacity_and_starts()
-        self._data["capacities"] = capacities
-        self._data["num_vehicles"] = len(capacities)
-        self._data["starts"] = starts
-        self._data["ends"] = [
-            self.solution_request["destination"] for i in starts]
+        self._location_names = self.distance_matrix["destination_addresses"]
+        self._dm = self._calculate_matrix()
+        self._demands = self._calculate_demands()
+        starts, capacities, driver_ids = self._calculate_vehicle_info()
+        self._starts = starts
+        self._capacities = capacities
+        self._driver_ids = driver_ids
+        self._num_vehicles = len(capacities)
+        self._ends = [self.solution_request["destination"] for i in starts]
 
-        self._solution = self._solve()
+        self._manager, self._routing, self._solution = self._solve()
 
     def _calculate_matrix(self):
         """
@@ -69,7 +69,7 @@ class Routing:
             self.demands[person["location"]] += 1
         return self.demands
 
-    def _calculate_capacity_and_starts(self):
+    def _calculate_vehicle_info(self):
         """
         Calculates capacities of vehicles and starting locations.
 
@@ -78,11 +78,13 @@ class Routing:
         """
         capacities = []
         starts = []
+        driver_ids = []
         for p in self.solution_request["people"]:
             if p["capacity"] > 0:
                 capacities.append(p["capacity"])
                 starts.append(p["location"])
-        return starts, capacities
+                driver_ids.append(p["id"])
+        return starts, capacities, driver_ids
 
     def _solve(self):
         """
@@ -91,22 +93,21 @@ class Routing:
         For more explanation see OR-Tools routing example:
         https://developers.google.com/optimization/routing/cvrp.
         """
-        data = self._data
         manager = pywrapcp.RoutingIndexManager(
-            len(data["distance_matrix"]),
-            data["num_vehicles"],
-            data["starts"],
-            data["ends"]
+            len(self._dm),
+            self._num_vehicles,
+            self._starts,
+            self._ends
         )
         routing = pywrapcp.RoutingModel(manager)
 
-        def distance_callback(index_a: int, index_b: int) -> int:
+        def distance_callback(from_index: int, to_index: int) -> int:
             """
             Returns distance between nodes in the numeric distance matrix.
             """
-            node_a = manager.IndexToNode(index_a)
-            node_b = manager.IndexToNode(index_b)
-            return self._data["distance_matrix"][node_a][node_b]
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return self._dm[from_node][to_node]
 
         transit_callback_index = routing.RegisterTransitCallback(
             distance_callback)
@@ -117,14 +118,14 @@ class Routing:
             Returns the demand of a node.
             """
             node = manager.IndexToNode(index)
-            return data["demands"][node]
+            return self._demands[node]
         
         demand_callback_index = routing.RegisterUnaryTransitCallback(
             demand_callback)
         routing.AddDimensionWithVehicleCapacity(
             demand_callback_index,
             0,  # null capacity slack
-            data["capacities"],  # vehicle maximum capacities
+            self._capacities,  # vehicle maximum capacities
             True,  # start cumul to zero
             "Capacity"
         )
@@ -135,30 +136,54 @@ class Routing:
         search_parameters.first_solution_strategy = (
             routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
 
-        self._solution = routing.SolveWithParameters(search_parameters)
+        solution = routing.SolveWithParameters(search_parameters)
         self._manager = manager
         self._routing = routing
+        return manager, routing, solution
 
-        self.print_solution()
+    def print_formulation(self):
+        """
+        Prints problem presented to optimiser to terminal.
+        """
+        print("\nDistance matrix:")
+        for i in self._dm:
+            print(i)
+
+        print("\nLocations and demands:")
+        for i_loc, loc in enumerate(self.solution_request["locations"]):
+            address = self.distance_matrix['destination_addresses'][i_loc]
+            print(f"{i_loc}: "
+                  f"Lat: {loc['lat']}, lgn: {loc['lgn']}, "
+                  f"name: {address}, "
+                  f"demand: {self._demands[i_loc]}")
+        
+        print("\nVehicles and drivers:")
+        for i, driver_id in enumerate(self._driver_ids):
+            name = self.solution_request['people'][driver_id]["name"]
+            location = self._location_names[self._starts[i]]
+            print(f"{i}: "
+                  f"name: {name}, "
+                  f"location: {location}, "
+                  f"capacity: {self._capacities[i]}")
+        print()
 
     def print_solution(self):
         """
         Prints solution to terminal.
         """
-        data = self._data
         manager = self._manager
         routing = self._routing
         solution = self._solution
         total_distance = 0
         total_load = 0
-        for vehicle_id in range(data["num_vehicles"]):
+        for vehicle_id in range(self._num_vehicles):
             index = routing.Start(vehicle_id)
             plan_output = "Route for vehicle {}:\n".format(vehicle_id)
             route_distance = 0
             route_load = 0
             while not routing.IsEnd(index):
                 node_index = manager.IndexToNode(index)
-                route_load += data["demands"][node_index]
+                route_load += self._demands[node_index]
                 plan_output += " {0} Load({1}) -> ".format(node_index, route_load)
                 previous_index = index
                 index = solution.Value(routing.NextVar(index))
@@ -173,10 +198,6 @@ class Routing:
             total_load += route_load
         print("Total distance of all routes: {}m".format(total_distance))
         print("Total load of all routes: {}".format(total_load))
-
-    @property
-    def data(self):
-        return self._data
 
     @property
     def solution(self):
--- tests/test_routing.py
+++ tests/test_routing.py
@@ -6,11 +6,10 @@ Test routing works as expected.
 import pytest
 from flask import Flask
 
-from app.routing import Routing
+from app.route_optimizer import RouteOptimizer
 
-
-# def test_plan_route(solution_request_1, distance_matrix_response):
-#     optimized_
 
 def test_routing_entry(solution_request_1, distance_matrix_response_1):
-    route = Routing(solution_request_1, distance_matrix_response_1)
+    route = RouteOptimizer(solution_request_1, distance_matrix_response_1)
+    route.print_formulation()
+    route.print_solution()
